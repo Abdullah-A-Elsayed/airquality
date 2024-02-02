4f924d1eeac88686a1bf353e137df603ad70c11c
Abdullah Aly <abdullah.ali26@hotmail.com>
2024-02-02 11:53:56 +0200

    adding PollutionData model

---
+++ src/server/models/v1/PollutionData.ts
@@ -0,0 +1,21 @@
+export class PollutionData {
+  ts: string;
+  aqius: number;
+  mainus: string;
+  aqicn: number;
+  maincn: string;
+
+  constructor(
+    ts: string,
+    aqius: number,
+    mainus: string,
+    aqicn: number,
+    maincn: string
+  ) {
+    this.ts = ts;
+    this.aqius = aqius;
+    this.mainus = mainus;
+    this.aqicn = aqicn;
+    this.maincn = maincn;
+  }
+}
