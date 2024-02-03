f0b82d77f9a20d9aa7204d41bfc3cd7526fae396
Abdullah Aly <abdullah.ali26@hotmail.com>
2024-02-03 03:10:51 +0200

    docs: /paris/max endpoint

--- src/server/controllers/v1/PollutionController.ts
+++ src/server/controllers/v1/PollutionController.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { PollutionService } from "../../services/PollutionService";
+import prisma from "../../../lib/db";
+import { PARIS_COORDINATES } from "../../../utils/constants";
 
 export const PollutionRouter = express.Router();
 
@@ -17,5 +19,30 @@ PollutionRouter.get("/", async (req, res, next) => {
     });
   } catch (err) {
     next(err);
+  }
+});
+
+PollutionRouter.get("/paris/max", async (req, res, next) => {
+  try {
+    const maxPollution = await prisma.pollution.findFirst({
+      where: {
+        latitude: PARIS_COORDINATES.latitude,
+        longitude: PARIS_COORDINATES.longitude,
+      },
+      orderBy: {
+        aqius: "desc",
+      },
+    });
+
+    if (maxPollution) {
+      res.json({ Result: { Datetime: maxPollution.ts } });
+    } else {
+      next({
+        message: "No pollution data found for Paris",
+        status: 404,
+      });
+    }
+  } catch (error) {
+    next(error);
   }
 });
